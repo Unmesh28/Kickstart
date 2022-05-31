edae21d37453500637f449f5817d0e8fc3ec2653
Unmesh28 <unmesh.dabhade@gmail.com>
2022-05-31 09:20:11 +0530

    requests show added

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  '0xA924aBB896210ADd2449B85625D169d54acdc9AD'
+);
+
+export default instance;
